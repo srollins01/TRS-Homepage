f8882450e14e294aa29aa42863662736c828d84b
Sarah Rollins <s.rollins751@gmail.com>
2024-08-31 00:01:20 +0900

    Added link to event card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -21,7 +21,7 @@ function Card({event}:Props) {
   }, [event]);
 
   return (
-    <>
+    <a href="https://www.meetup.com/rpgtokyo/" target="_blank">
       <div className="card">
         <div className='sk-meetup-event' data-embed-id='25455614'></div><script src='https://widgets.sociablekit.com/meetup-group-events/widget.js' async defer></script>
           <h2>{event["name"]}</h2>
@@ -49,7 +49,7 @@ function Card({event}:Props) {
           </div>
         </div>
       </div>
-    </>
+    </a>
   )
 }
 
